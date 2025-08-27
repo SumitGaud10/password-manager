1f471c57a3217c7a642ef6830daa74aa388106de
SumitGaud10 <srgthegreat2006@gmail.com>
2025-08-28 01:26:27 +0530

    middlware touchup

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,7 +3,11 @@ import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
 export async function middleware(req: NextRequest) {
-  const token = await getToken({ req, secret: process.env.AUTH_SECRET });
+  const token = await getToken({
+    req,
+    secret: process.env.AUTH_SECRET,
+    secureCookie: process.env.NODE_ENV === "production",
+  });
 
   const session = !!token;
   const { pathname } = req.nextUrl;
